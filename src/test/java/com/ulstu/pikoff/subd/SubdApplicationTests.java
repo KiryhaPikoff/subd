08f4f40806e026e78f291993a054584b0df2b7d2
Kiryha <pikov_kirya@mail.ru>
2020-03-27 07:56:42 +0300

    init

---
+++ src/test/java/com/ulstu/pikoff/subd/SubdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulstu.pikoff.subd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
